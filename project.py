0bb2db7c6c7b817322a6271cd903c9ab8956d968
DianSeo <qudand50@gmail.com>
2025-08-19 18:06:57 +0900

    test

--- project.py
+++ project.py
@@ -2,3 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+
