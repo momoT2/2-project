20722cc7f8caa74576eb1180590b101cea47f3f3
DianSeo <qudand50@gmail.com>
2025-08-20 12:16:45 +0900

    test

--- project.py
+++ project.py
@@ -8,4 +8,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
-
+FILE_ID = "1pDUe-oXGh_mqAORcGKKYIBkemNUMDVtC"
+GID = "0"  
+xlsx_url = f"https://docs.google.com/spreadsheets/d/{FILE_ID}/export?format=xlsx"
+df = pd.read_excel(xlsx_url, engine="openpyxl")  
+print(df.head())
+print(df.info())
