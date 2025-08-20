e15442062929472e8e40e18123cb7cf283128a32
DianSeo <qudand50@gmail.com>
2025-08-20 12:50:33 +0900

    test

--- project.py
+++ project.py
@@ -14,3 +14,13 @@ xlsx_url = f"https://docs.google.com/spreadsheets/d/{FILE_ID}/export?format=xlsx
 df = pd.read_excel(xlsx_url, engine="openpyxl")  
 print(df.head())
 print(df.info())
+
+X = df.drop(columns=["id", "burnout60"])
+y = df["burnout60"]
+
+from sklearn.preprocessing import LabelEncoder
+for col in X.select_dtypes(include=["object"]).columns:
+    X[col] = LabelEncoder().fit_transform(X[col].astype(str))
+
+print(X.head())
+print(X.info())
