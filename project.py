42bdbe884455ad58413f4281dc9a0466075c86a8
DianSeo <qudand50@gmail.com>
2025-08-22 18:03:57 +0900

    0822

--- project.py
+++ project.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import statsmodels.api as sm
 from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
@@ -24,3 +23,13 @@ for col in X.select_dtypes(include=["object"]).columns:
 
 print(X.head())
 print(X.info())
+
+X = df.iloc[:, 0:-1]
+y = df["burnout60"]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                    test_size=0.3, 
+                                                    random_state=42)
+print("train 데이터: ", X_train.shape)
+print("test 데이터: ", X_test.shape)
